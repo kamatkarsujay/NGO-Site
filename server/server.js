6d295d7cf96c9a91c566d4d0aedc2eddefa4dd05
Sujay Kamatkar <sujaykamatkar382@gmail.com>
2023-03-14 19:30:13 +0530

    made some changes

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://ngositereact.netlify.app/",
+    origin: "*",
     credentials: true,
   })
 );
